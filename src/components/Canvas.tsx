423f9019bfdf8c7460929d9a0c7467a85a2be997
markitosha <nataly_106@mail.ru>
2024-11-17 19:35:11 +0100

    Removed controls

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -7,7 +7,7 @@ export default function Canvas() {
 
     return <div className={'flex items-center'}>
         <div className={'relative'}>
-            <video src={'https://firebasestorage.googleapis.com/v0/b/pleaz-player-staging.appspot.com/o/282406_half_2_10min.mp4?alt=media&token=a47d6f04-cbaf-4521-a623-9724f5f45738'} controls className={'w-2/3'} ref={videoRef}/>
+            <video src={'https://firebasestorage.googleapis.com/v0/b/pleaz-player-staging.appspot.com/o/282406_half_2_10min.mp4?alt=media&token=a47d6f04-cbaf-4521-a623-9724f5f45738'} className={'w-2/3'} ref={videoRef}/>
             <canvas width={sizes.videoWidth} height={sizes.videoHeight} className={'absolute top-0 left-0'} style={{
                 width: sizes.width,
                 height: sizes.height,
